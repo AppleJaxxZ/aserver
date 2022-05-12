b730d4c618265c82f126cd7d68bc86a708809ba5
Bryan A <easyrun32@gmail.com>
2022-05-11 20:50:36 -0400

    aws

--- controller/subscription.controller.js
+++ controller/subscription.controller.js
@@ -3,38 +3,38 @@ const { subscriptionService } = require('../service/');
 const stripe = require('stripe')(stripe_secret);
 var AWS = require('aws-sdk');
 
-
-AWS.config.update({ accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_KEY, region: 'us-east-1' });
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: 'us-east-1',
+});
 const lambda = new AWS.Lambda();
-
-
 
 const getAllActiveSubscriptions = async (req, res) => {
   const activeSubscriptions =
     await subscriptionService.getAllActiveSubscriptions();
 
   try {
-    await activeSubscriptions.forEach(user => {
-      console.log(user)
+    await activeSubscriptions.forEach((user) => {
+      console.log(user);
       const params = {
-        FunctionName: 'newestScraper', /* required */
+        FunctionName: 'newestScraper' /* required */,
         Payload: JSON.stringify({
           pinNumber: user.pinNumber,
           phoneNumber: user.phoneNumber,
           dateOfBirth: user.dateOfBirth,
-        })
+        }),
       };
-      console.log(params.Payload)
+      console.log(params.Payload);
       lambda.invoke(params, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data); return data;         // successful response
-      })
-    })
+        else console.log(data);
+        return data; // successful response
+      });
+    });
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-
-
 
   res.send(activeSubscriptions);
 };
